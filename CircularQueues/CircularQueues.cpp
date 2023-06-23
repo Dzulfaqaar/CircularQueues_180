0e9cc31dff95146084c2a549fc0313174f4df84e
dzulfaqaar <fikri6aul@gmail.com>
2023-06-23 09:32:41 +0700

    void insert

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -4,4 +4,23 @@ using namespace std;
 class Queues {
 	int FRONT, REAR, max = 5;
 	int queues_array[5]; 
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number:";
+		cin >> num;
+		cout << endl;
+
+		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueues overflow\n";
+			return;
+		}
+	}
+};
 
